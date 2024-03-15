ee4841fb8d6587ab90d0dbf410f93ab032023cba
Stan Stanislaus <stan.stanislaus@steffesgroup.com>
2024-03-15 14:48:47 -0500

    fix: make script work even if client side routing
    That is, don't require full refresh of page to function

--- azureDevopsPRs-hotkeys.user.js
+++ azureDevopsPRs-hotkeys.user.js
@@ -5,58 +5,39 @@
 // @description  Keyboard shortcuts for Azure Devops PRs
 // @author       Stan-Stani
 // @homepage     https://www.seldoncortex.com/
-// @match        */pullrequest/*
-// @include       */pullrequest/*
+// @match        **dev.azure**
+// @match        **visualstudio**
+// @include      **dev.azure**
+// @include      **visualstudio**
 // @icon         https://www.seldoncortex.com/favicon.ico
 // @grant        none
 // ==/UserScript==
 
-;(function () {
-  "use strict"
-  const showOnlyActiveComments = (event) => {
-    if (
-      event.key === "ArrowRight" &&
-      event.ctrlKey &&
-      document.activeElement.nodeName !== "TEXTAREA" &&
-      document.activeElement.nodeName !== "INPUT"
-    ) {
-      const nodeArr = Array.from(
-        document.querySelectorAll(
-          ".bolt-button, .enabled, .bolt-focus-treatment"
-        )
-      ).filter((x) => x.innerText && x.innerText.includes("Show everything"))
-      nodeArr[0].click()
-      const nodeArrTwo = Array.from(
-        document.querySelectorAll("#__bolt-active_comments")
-      ).filter((x) => x.innerText && x.innerText.includes("Active comments"))
-      nodeArrTwo[0].click()
-      console.log(nodeArrTwo)
+(function() {
+    'use strict';
+    const showOnlyActiveComments = (event) => {
+        if (event.key === 'ArrowRight' && event.ctrlKey && document.activeElement.nodeName !== "TEXTAREA" && document.activeElement.nodeName !== "INPUT") {
+        const nodeArr = Array.from(document.querySelectorAll('.bolt-button, .enabled, .bolt-focus-treatment')).filter((x) => x.innerText && x.innerText.includes('Show everything'))
+        nodeArr[0].click()
+        const nodeArrTwo = Array.from(document.querySelectorAll('#__bolt-active_comments')).filter((x) => x.innerText && x.innerText.includes('Active comments'))
+        nodeArrTwo[0].click()
+        console.log(nodeArrTwo)
+        }
     }
-  }
 
-  const showAllComments = (event) => {
-    if (
-      event.key === "ArrowLeft" &&
-      event.ctrlKey &&
-      document.activeElement.nodeName !== "TEXTAREA" &&
-      document.activeElement.nodeName !== "INPUT"
-    ) {
-      const nodeArr = Array.from(
-        document.querySelectorAll(
-          ".bolt-button, .enabled, .bolt-focus-treatment"
-        )
-      ).filter((x) => x.innerText && x.innerText.includes("Active comments"))
-      nodeArr[0].click()
+     const showAllComments = (event) => {
+        if (event.key === 'ArrowLeft' && event.ctrlKey && document.activeElement.nodeName !== "TEXTAREA" && document.activeElement.nodeName !== "INPUT") {
+        const nodeArr = Array.from(document.querySelectorAll('.bolt-button, .enabled, .bolt-focus-treatment')).filter((x) => x.innerText && x.innerText.includes('Active comments'))
+        nodeArr[0].click()
 
-      const nodeArrTwo = Array.from(
-        document.querySelectorAll("#__bolt-everything")
-      ).filter((x) => x.innerText && x.innerText.includes("Show everything"))
-      console.log(nodeArrTwo)
-      nodeArrTwo[0].click()
-      console.log(nodeArrTwo)
+        const nodeArrTwo = Array.from(document.querySelectorAll('#__bolt-everything')).filter((x) => x.innerText && x.innerText.includes('Show everything'))
+        console.log(nodeArrTwo)
+        nodeArrTwo[0].click()
+        console.log(nodeArrTwo)
+        }
     }
-  }
 
-  document.addEventListener("keyup", showOnlyActiveComments, false)
-  document.addEventListener("keyup", showAllComments, false)
-})()
+
+    document.addEventListener('keyup', showOnlyActiveComments, false);
+    document.addEventListener('keyup', showAllComments, false);
+    })();
